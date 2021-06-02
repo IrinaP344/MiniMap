335cc69276dd1fee4305a27a4e43f0c118bfd6c4
IrinaP344 <71124960+IrinaP344@users.noreply.github.com>
2021-06-02 09:41:55 +0900

    Add files via upload

---
+++ playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    float x;
+    float y;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        rb.AddForce(transform.forward * y * 20f);
+
+        transform.Rotate(0,x,0);
+    }
+}
